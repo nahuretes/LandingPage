49b772e30aae42eeb5764ccdf103642f9ed49369
Nahu Retes <nahueretes@gmail.com>
2022-04-21 01:33:59 -0300

    V0.0.4

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 200,
+        "value": 25,
         "density": {
           "enable": true,
           "value_area": 800
